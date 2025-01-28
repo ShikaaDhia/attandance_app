def5b80e522a34d271b75675853013909c43b952
ShikaaDhia <shkaadhier@gmail.com>
2025-01-28 09:46:47 +0700

    config attendance_service file

--- lib/services/attendance_service.dart
+++ lib/services/attendance_service.dart
@@ -1,8 +1,69 @@
+import 'package:attandance_app/ui/home_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+// an entry point for submitting the attendance report
 final CollectionReference dataCollection = FirebaseFirestore.instance.collection('attendance');
 
-Future<void> submitReport(BuildContext context, String address, String name, String status, String timeStamp) async {
-//  showLoaderDialog(context);
+Future<void> submitAttendanceReport(BuildContext context, String address, String name, String attendanceStatus, String timeStamp) async {
+ showLoaderDialog(context);
+ dataCollection.add(
+  {
+    'address': address,
+    'name': name,
+    'attendanceStatus': attendanceStatus,
+    'timeStamp': timeStamp
+  }
+ ).then((result) {
+  Navigator.of(context).pop();
+  try {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              Icons.check_circle_outline,
+              color: Colors.white,
+            ),
+            SizedBox(width: 10),
+            Text(
+              "Attendance submitted successfully",
+              style: TextStyle(color: Colors.white),
+            )
+          ]
+        ),
+        backgroundColor: Colors.orangeAccent,
+        shape: StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const HomeScreen()),
+    );
+  }
+ });
+}
+
+void showLoaderDialog(BuildContext context) {
+  AlertDialog alert = AlertDialog(
+    content: Row(
+      children: [
+      const CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.blueAccent),
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 20),
+        child: const Text("Checking the data..."),
+      )
+    ],
+    ),
+  );
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
