cb65c625d989ab9e38b4a019f2932b905139f489
ShikaaDhia <shkaadhier@gmail.com>
2025-01-29 08:55:20 +0700

    finishing attendance service

--- lib/services/attendance_service.dart
+++ lib/services/attendance_service.dart
@@ -41,7 +41,49 @@ Future<void> submitAttendanceReport(BuildContext context, String address, String
       context,
       MaterialPageRoute(builder: (context) => const HomeScreen()),
     );
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Row(
+        children: [
+          const Icon(
+            Icons.info_outline,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 10),
+          Expanded(child: Text(
+            "Ups!, $e",
+            style: const TextStyle(color: Colors.white)
+            // TODO - UNTIL HERE XIXI
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Colors.blueAccent,
+      shape: const StadiumBorder(),
+      behavior: SnackBarBehavior.floating,
+    ));
   }
+ }).catchError((error) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Row(
+      children: [
+        const Icon(
+          Icons.error_outline,
+          color: Colors.white,
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            "Ups!, $error",
+            style: const TextStyle(color: Colors.white),
+          ),
+        )
+      ],
+    ),
+    backgroundColor: Colors.blueAccent,
+    shape: const StadiumBorder(),
+    behavior: SnackBarBehavior.floating,
+  ));
  });
 }
 
