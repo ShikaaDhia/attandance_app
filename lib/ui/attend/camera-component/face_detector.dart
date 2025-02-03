bf7d7328dff4ab25ebad36db38c3c08958c3620d
ShikaaDhia <shkaadhier@gmail.com>
2025-02-03 10:56:45 +0700

    creating face_detector feature

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,3 +1,13 @@
+import 'package:attandance_app/utils/google_ml_kit.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
 class FaceDetectorComponent {
-  
+  final FaceDetector faceDetector = GoogleMlKit.vision.faceDetector(
+    FaceDetectorOptions(
+      enableClassification: true,
+      enableTracking: true,
+      enableContours: true,
+      enableLandmarks: true,
+    ),
+  );
 }
