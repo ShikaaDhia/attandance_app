b818b9ce8abd1bd231bd83d43f58200344aad5cb
ShikaaDhia <shkaadhier@gmail.com>
2025-02-03 08:46:43 +0700

    finishing attendance feature

--- lib/ui/attend/attendance_screen.dart
+++ lib/ui/attend/attendance_screen.dart
@@ -1,5 +1,11 @@
 import 'package:attandance_app/services/location_service.dart';
 import 'package:attandance_app/services/timestamp_service.dart';
+import 'package:attandance_app/ui/attend/components/app_bar.dart';
+import 'package:attandance_app/ui/attend/components/capture_photo.dart';
+import 'package:attandance_app/ui/attend/components/header.dart';
+import 'package:attandance_app/ui/attend/components/location.dart';
+import 'package:attandance_app/ui/attend/components/name_input.dart';
+import 'package:attandance_app/ui/attend/components/submit_button.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +22,7 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
   _AttendanceScreenState(this.image);
 
   XFile? image;
-  String address = "", datePlaceholder = "", timePlaceholder = "", timeStampPlaceholder = "", statusPlaceholder = "Attend";
+  String addressPlaceholder = "", datePlaceholder = "", timePlaceholder = "", timeStampPlaceholder = "", statusPlaceholder = "Attend";
   bool isLoading = false;
   // double lat = 0.0, long = 0.0;
   // int dateHours = 0;
@@ -34,10 +40,54 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
         timeStampPlaceholder = timeStamp;
       });
     });
+    setAttendStatus((status) {
+      setState(() {
+        statusPlaceholder = status;
+      });
+    });
+
+    if (image != null) {
+     isLoading = true;
+     getGeoLocationPosition(context, (position) {
+      setState(() {
+        isLoading = false;
+        getAddressFromLongLat(position, (address) {
+          setState(() {
+            addressPlaceholder = address;
+          });
+        });
+      });
+     });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
+      body: SingleChildScrollView(
+        child: Card(
+          color: Colors.white,
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+          elevation: 5,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildHeader(),
+              buildCapturePhotoSection(context, size, image),
+              buildNameInput(controllerName),
+              buildLocationSection(isLoading, addressPlaceholder),
+              buildSubmitButton(context, size, image, controllerName, addressPlaceholder, statusPlaceholder, timeStampPlaceholder)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
