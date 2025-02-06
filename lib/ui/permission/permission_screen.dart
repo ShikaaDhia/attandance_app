2aad6b8997fb064cb8874382ca9218fe6401944c
ShikaaDhia <shkaadhier@gmail.com>
2025-02-06 13:31:59 +0700

    creating form header placeholder

--- lib/ui/permission/permission_screen.dart
+++ lib/ui/permission/permission_screen.dart
@@ -1,6 +1,5 @@
-import 'package:attandance_app/services/location_service.dart';
+import 'package:attandance_app/ui/permission/components/app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class PermissionScreen extends StatefulWidget {
   const PermissionScreen({super.key});
@@ -12,16 +11,32 @@ class PermissionScreen extends StatefulWidget {
 class _PermissionScreenState extends State<PermissionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
+      body: SingleChildScrollView(
+        child: Card(
+          color: Colors.white,
+          margin: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 5,
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // buat komponen UI untk form header
+              // buat komponen UI untk form body
+              // buat komponen UI untk form submit button
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  void requestPermission() async {
-    final hasPermission = await handleLocationPermission(context);
-
-    try {
-      
-    } catch (e) {
-      
-    }
-  }
+  
 }
