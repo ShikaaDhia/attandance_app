d32e6655fea7240b7a8df21b07245f80ffd51cf7
ShikaaDhia <shkaadhier@gmail.com>
2025-02-06 13:14:05 +0700

    finishing app bar for permission

--- lib/ui/attend/camera-component/face_detector.dart
+++ lib/ui/attend/camera-component/face_detector.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:attandance_app/utils/google_ml_kit.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class FaceDetectorComponent {
@@ -15,5 +18,40 @@ class FaceDetectorComponent {
   Future<void> detectFaces(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
     final faces = await faceDetector.processImage(inputImage);
+
+    // proses faces nya udh terdeteksi
+    for (Face face in faces) {
+      // rect : rectangle (buat ngambil posisi wajah)
+      // boundingbox : buat ngambil beberapa komponen dri wajah (position)
+      final Rect boundingBox = face.boundingBox;
+
+      // untk menghandle posisi wajah secara vertikal
+      final double? verticalPosition = face.headEulerAngleY;
+
+      // untk menghandle posisi wajah secara horizontal
+      final double? horizontalPosition = face.headEulerAngleZ;
+
+      // perkondisian apabila face landmark sudah aktif, ditandai oleh (mulut, mata, pipi, hidung, dan telinga)
+      final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
+      // klo pnya kuping dia bakal ngembaliin posisi letak kupingnya
+      if (leftEar != null) {
+        final Point<int> leftEarPosition = leftEar.position;
+        print('Left Ear Position: $leftEarPosition');
+        
+      }
+
+      // perkondisian apabila wajahnya terdeteksi (ditandai dengan bibir tersenyum)
+      if (face.smilingProbability != null) {
+        final double? smilingProbability = face.smilingProbability;
+        print("Smiling Probability: $smilingProbability");
+      }
+
+      //perkondisian apabila fitur tracking wajah sudah aktif
+      if (face.trackingId != null) {
+        final int? trackingId = face.trackingId;
+        print("Tracking ID: $trackingId");
+      }
+
+    }
   }
-}
+} 
--- lib/ui/permission/components/error_handler.dart
+++ lib/ui/permission/components/error_handler.dart
@@ -1,6 +1,7 @@
+// menampilkan sebuah error dengan cara kejelasan
+
 import 'package:attandance_app/ui/permission/components/snack_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
 class ErrorHandler {
   static void handleError(BuildContext context, dynamic error) {
@@ -19,5 +20,14 @@ class ErrorHandler {
     }
     Navigator.of(context).pop();
   }
-  // TODO: implement exception handling, if the permission is denied
+}
+
+class PermissionDeniedException implements Exception {
+  final String message;
+
+// proses pembuatan placeholder dri sebuah exception/error
+  PermissionDeniedException([this.message = '']);
+
+  @override
+  String toString() => message.isEmpty ? "Permission Denied" : message;
 }
