458259777b85b5dae5ee1a914e8755b87ab1fe9b
ShikaaDhia <shkaadhier@gmail.com>
2025-02-06 15:01:31 +0700

    finishing permission feature

--- lib/ui/permission/permission_screen.dart
+++ lib/ui/permission/permission_screen.dart
@@ -1,4 +1,8 @@
 import 'package:attandance_app/ui/permission/components/app_bar.dart';
+import 'package:attandance_app/ui/permission/components/form_body.dart';
+import 'package:attandance_app/ui/permission/components/form_header.dart';
+import 'package:attandance_app/ui/permission/components/submit_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class PermissionScreen extends StatefulWidget {
@@ -25,12 +29,22 @@ class _PermissionScreenState extends State<PermissionScreen> {
             borderRadius: BorderRadius.circular(10),
           ),
           elevation: 5,
-          child: const Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // buat komponen UI untk form header
-              // buat komponen UI untk form body
-              // buat komponen UI untk form submit button
+              const FormHeader(
+                title: "Permission Request Form",
+                icon: Icons.assignment,
+              ),
+              const FormBody(),
+              SubmitButton(
+                size: size,
+                nameController: TextEditingController(),
+                formController: TextEditingController(),
+                toController: TextEditingController(),
+                dropValueCategories: "Dropdown Content",
+                dataCollection: FirebaseFirestore.instance.collection('dataCollectionValue'),
+              )
             ],
           ),
         ),
